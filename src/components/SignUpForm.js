df8c41b2ff1b7c29663cd5b60a4c639aa95ce6dc
Chad Christensen <chadchristensen2@gmail.com>
2017-09-16 14:25:58 -0700

    Add re-enter password box to signup form and consolidate name input

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -6,10 +6,10 @@ class SignUpForm extends React.Component {
     return(
       <div className="container">
         <form ref={(input) => this.SignUpForm = input} className="product-edit">
-          <input ref={(input) => this.firstName = input} type="text" placeholder="First Name"/>
-          <input ref={(input) => this.lastName = input} type="text" placeholder="Last Name"/>
+          <input ref={(input) => this.firstName = input} type="text" placeholder="Name"/>
           <input ref={(input) => this.email = input} type="text" placeholder="Email Address"/>
           <input ref={(input) => this.password = input} type="text" placeholder="Password"/>
+          <input ref={(input) => this.password = input} type="text" placeholder="Re-enter Password"/>
           <button type="submit">Sign Up</button>
         </form>
       </div>
