e3850f13fcfad24f20061ccad88850de78de00d1
Chad Christensen <chadchristensen2@gmail.com>
2017-10-08 07:22:22 -0700

    Convert signup and login form to use controlled components

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -2,14 +2,51 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class LogInForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      password: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    let target = event.target,
+        name = target.name,
+        value = target.value;
+
+        this.setState({
+          [name]: value
+        });
+  }
+
+  handleSubmit() {
+    console.log(this.state)
+  }
 
   render() {
     return(
       <div className="container">
         <h2>Welcome back.</h2>
         <form ref={(input) => this.SignUpForm = input} className="product-edit">
-          <input ref={(input) => this.email = input} type="text" placeholder="Email Address"/>
-          <input ref={(input) => this.password = input} type="text" placeholder="Password"/>
+          <input
+            name="name"
+            type="text"
+            placeholder="Email Address"
+            onChange={this.handleInputChange}
+            value={this.state.name}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.handleInputChange}
+            value={this.state.password}
+          />
           <button type="submit" className="btn">Log In</button>
           <p>Don't have an account? <Link to="/signup">Sign Up.</Link></p>
         </form>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,16 +2,66 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    let target = event.target,
+        name = target.name,
+        value = target.value;
+
+        this.setState({
+          [name]: value
+        });
+  }
+
+  handleSubmit() {
+    console.log(this.state)
+  }
 
   render() {
     return(
       <div className="container">
         <h2>Sign up Now</h2>
-        <form ref={(input) => this.SignUpForm = input} className="product-edit">
-          <input ref={(input) => this.firstName = input} type="text" placeholder="Name"/>
-          <input ref={(input) => this.email = input} type="text" placeholder="Email Address"/>
-          <input ref={(input) => this.password = input} type="password" placeholder="Password"/>
-          <input ref={(input) => this.password = input} type="password" placeholder="Re-enter Password"/>
+        <form onSubmit={this.handleSubmit} className="product-edit">
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            onChange={this.handleInputChange}
+            value={this.state.name}
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="Email Address"
+            onChange={this.handleInputChange}
+            value={this.state.email}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={this.handleInputChange}
+            value={this.state.password}
+          />
+          <input
+            name="confirmPassword"
+            type="password"
+            placeholder="Re-enter Password"
+            onChange={this.handleInputChange}
+            value={this.state.confirmPassword}
+          />
           <button type="submit" className="btn">Sign Up</button>
           <p>Already have an account? <Link to="/login">Log in.</Link></p>
         </form>
